61df52514beed3d42ce4aba435f81160d658dec0
Leo Harada <leotharada@gmail.com>
2023-09-17 18:01:27 -0700

    added backend starter. AGoing to work on Database now.

--- frontend/src/page/SignUp.js
+++ frontend/src/page/SignUp.js
@@ -44,13 +44,26 @@ const SignUp = () => {
         });
     };
 
-    const handleSubmit = (e) => {
+    console.log(process.env.REACT_APP_SERVER_DOMAIN);
+    const handleSubmit = async (e) => {
         e.preventDefault();
         const { firstName, email, password, confirmPassword } = data;
         if (firstName && email && password && confirmPassword) {
             if (password === confirmPassword) {
+                const fetchData = await fetch(
+                    `${process.env.REACT_APP_SERVER_DOMAIN}/signup`,
+                    {
+                        method: "POST",
+                        headers: {
+                            "content-type": "application/json",
+                        },
+                        body: JSON.stringify(data),
+                    }
+                );
+                const dataRes = await fetchData.json();
+                console.log(dataRes);
                 alert("Successful!");
-                navigate("/login");
+                // navigate("/login");
             } else {
                 alert("Double check your password!");
             }
